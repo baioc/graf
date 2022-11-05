17b4950116092e9815e3fc5652d301dad1f56d20
Gabriel B. Sant'Anna <baiocchi.gabriel@gmail.com>
2022-11-05 16:13:38 -0300

    Improve release script

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-docker build -t dotnet:7.0-bionic .
-
-docker run -it --rm \
-	-v "$(pwd)":/source -w /source \
-	dotnet:7.0-bionic \
-	/bin/bash -c "/opt/dotnet publish -c Release -o build/ && chown -R $(id -u):$(id -g) ."
---
+++ make-release.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -e
+
+# parameterize
+TARGET='linux-x64'
+if [ -z "$1" ]; then
+	echo "Must specify a version"; exit 1
+else
+	VERSION="$1";
+fi
+
+# prepare
+docker build -t dotnet:7.0-bionic .
+
+# compile
+docker run -it --rm \
+	-v "$(pwd)":/source -w /source \
+	dotnet:7.0-bionic \
+	/bin/bash -c "/opt/dotnet publish -c Release -o build/ && chown -R $(id -u):$(id -g) ."
+
+# package
+cd build
+tar -czf graf-$VERSION-$TARGET.tar.gz graf
+sha256sum graf-$VERSION-$TARGET.tar.gz > graf-$VERSION-$TARGET.tar.gz.sha256
+cd ..
